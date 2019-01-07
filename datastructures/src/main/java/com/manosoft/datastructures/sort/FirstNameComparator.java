007ef892eef2276d968c16e212f85f2ffe2ca953
Manohar R Mandadi <manohar_mandadi@dell.com>
2019-01-06 22:52:16 -0800

    Class level comments added

--- datastructures/src/main/java/com/manosoft/datastructures/sort/FirstNameComparator.java
+++ datastructures/src/main/java/com/manosoft/datastructures/sort/FirstNameComparator.java
@@ -4,6 +4,9 @@ import java.util.Comparator;
 
 import com.manosoft.datastructures.model.Person;
 
+/**
+Confliecting clss level comments
+*/
 public class FirstNameComparator implements Comparator<Person> {
 
 	public int compare(Person p1, Person p2) {
