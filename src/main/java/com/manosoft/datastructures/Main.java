4ccef340ffd87ed769484deaae0bae3773a25e97
Manohar R Mandadi <manohar.mandadi@gmail.com>
2025-02-28 19:12:24 +0530

    Configure Log4j2. Add Main class

---
+++ src/main/java/com/manosoft/datastructures/Main.java
@@ -0,0 +1,13 @@
+package com.manosoft.datastructures;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Starting Main App");
+    }
+}
